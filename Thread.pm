7474db0d9ca4b1b3de686dd80919ba7ee21f4a01
jdhedden <jdhedden@cpan.org>
2016-04-21 10:06:36 -0400

    Bundle-Thread v1.13

--- Thread.pm
+++ Thread.pm
@@ -3,7 +3,7 @@ package Bundle::Thread;
 use strict;
 use warnings;
 
-our $VERSION = '1.09';
+our $VERSION = '1.13';
 $VERSION = eval $VERSION;
 
 1;
@@ -20,13 +20,13 @@ Bundle::Thread - A bundle of modules for supporting threads in Perl
 
 =head1 CONTENTS
 
-Test::Simple 0.80               - Used for module testing
+Test::Simple 0.94               - Used for module testing
 
-Scalar::Util 1.19               - Used by several of the other modules below
+Scalar::Util 1.23               - Used by several of the other modules below
 
-threads 1.71                    - Support for threads
+threads 1.76                    - Support for threads
 
-threads::shared 1.22            - Support for sharing objects between threads
+threads::shared 1.33            - Support for sharing objects between threads
 
 Thread::Semaphore 2.09          - Thread-safe semaphores
 
@@ -46,7 +46,7 @@ Jerry D. Hedden, S<E<lt>jdhedden AT cpan DOT orgE<gt>>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008 Jerry D. Hedden. All rights reserved.
+Copyright 2008, 2009 Jerry D. Hedden. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
